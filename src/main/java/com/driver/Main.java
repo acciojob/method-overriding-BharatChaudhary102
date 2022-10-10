a1e2d1673581f3e97561eb81e9e842600c074d84
BharatChaudhary1002 <chaudahry.bharatbhai.28@ldce.ac.in>
2022-10-10 20:45:14 +0530

    Making class seperate

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,22 @@ package com.driver;
 
 public class Main {
 
-    static class A {
-        public String meth(){
-            return "Invoking method from class A";
-        }
-    }
-    static class B extends A {
-
-        @Override
-        public String meth(){
-            return "Method is overridden in Extendend class B";
-        }
-    }
     public static void main(String[] args) {
         B ob = new B();
         System.out.println(ob.meth());
     }
+}
 
+class A {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+
+    @Override
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
 }
 
